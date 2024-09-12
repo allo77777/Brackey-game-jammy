094e46a218c6b8784994a527b236d823928015ba
allo77777 <ahmedsamaoui832@gmail.com>
2024-09-12 17:20:19 -0500

    added death sound

--- brackeys_game_jam/scripts/killzone.gd
+++ brackeys_game_jam/scripts/killzone.gd
@@ -3,11 +3,14 @@ extends Area2D
 @export_range(0.1, 5) var death_time: float = 0.1
 
 @onready var timer: Timer = $Timer
+@onready var death_sfx = $death_sound
 
 func _ready() -> void:
 	timer.wait_time = death_time
 
 func _on_body_entered(body: Node2D) -> void:
+	death_sfx.play()
+	await death_sfx.finished
 	timer.start()
 
 func _on_timer_timeout() -> void:
